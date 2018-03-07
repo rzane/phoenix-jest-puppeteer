6a8531ed577fcc35d41835a7bf6bf6dd1c044cf0
Ray Zane <ray@promptworks.com>
2018-03-07 13:29:48 -0500

    Initial commit

---
+++ lib/blog_web/router.ex
@@ -0,0 +1,28 @@
+defmodule BlogWeb.Router do
+  use BlogWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", BlogWeb do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+
+    resources "/posts", PostController, except: [:new, :edit]
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", BlogWeb do
+  #   pipe_through :api
+  # end
+end
